661a4e8ce2ff3daf5c06c3c5f727f2d8e35062c3
hecter_wu <hecter_wu@transcend.com.tw>
2017-08-21 16:39:27 +0800

    refactor: O(log(n+m)) solution using divide and conquer and binary search

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -25,12 +25,37 @@ class unitest(unittest.TestCase):
 
 class Solution():
     def findMedianSortedArrays(self, nums1, nums2):
-        a = list(heapq.merge(nums1,nums2))
-        mid = int(len(a)/2)
-        if len(a)%2:
-            return a[mid]
-        else:
-            return (float(a[mid-1]) + float(a[mid]))/2
-
+        m, n = len(nums1),len(nums2)
+        # let n > m
+        if m > n:
+            nums1, nums2, m, n = nums2, nums1, n, m
+        imin, imax, halfLen = 0, m, (n + m + 1) / 2
+        # i+j == m-i+n-j+1
+        while imin <= imax:
+            i = int((imin + imax)/2)
+            j = int(halfLen - i)
+            # j == 0 or i == m or B[j-1] <= A[i]
+            if i < m and nums2[j-1] > nums1[i]:
+                imin = i + 1
+            # i == 0 or j = n or A[i-1] <= B[j]
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                imax = i - 1
+            else:
+                if i == 0:
+                    max_of_left = nums2[j-1]
+                elif j == 0:
+                    max_of_left = nums1[i-1]
+                else:
+                    max_of_left = max(nums1[i-1],nums2[j-1])
+                # m+n is odd
+                if (m + n) % 2 == 1:
+                    return max_of_left
+                if i == m:
+                    min_of_right = nums2[j]
+                elif j == n:
+                    min_of_right = nums1[i]
+                else:
+                    min_of_right = min(nums1[i],nums2[j])
+                return (max_of_left + min_of_right) / 2.0
 if __name__ == '__main__':
     unittest.main()
